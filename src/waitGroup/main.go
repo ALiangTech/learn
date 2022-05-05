6f15db122af55b49e79771312dc4f6d6890d9f5e
alinag <1305544590@qq.com>
2022-05-05 23:09:06 +0800

    协程操作

---
+++ src/waitGroup/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// 等待多个协程完成
+
+func worker(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+	wg.Wait()
+}
